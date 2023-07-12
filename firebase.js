b77d3603a87149367c5d9f18e5b45d83c60832c9
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-12 13:18:13 +0530

    auth domain change

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 
   var firebaseConfig = {
     apiKey : "AIzaSyAmtwzy1Vl86kL6O-mOxXGBmzvjXe-sils",
-    authDomain: "self-nasu.github.io",
+    authDomain: "naman-jain-p1.firebaseapp.com",
     projectId: "naman-jain-p1",
     // ... (other credentials)
   };
